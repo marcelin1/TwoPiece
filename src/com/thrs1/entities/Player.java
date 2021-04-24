6737c069daad5825a646fe0cd0e41dfce9cec58f
Diego <Diego@DESKTOP-S9GD7ID>
2021-04-24 00:58:50 -0300

    Commit M2

--- src/com/thrs1/entities/Player.java
+++ src/com/thrs1/entities/Player.java
@@ -1,5 +1,6 @@
 package com.thrs1.entities;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 import com.thrs1.main.Game;
@@ -9,9 +10,13 @@ import com.thrs1.main.Game;
 public class Player extends Entity {
 	
 	public boolean right,left,up,down;
+	public int dir_right = 0,dir_left = 1;
+	public int dir = dir_right;
 	public double speed = 1.5;
 	
-	private int frames = 0,maxFrames = 5,index = 0,maxIndex = 3;
+	private boolean moved = false;
+	
+	private int frames = 0,maxFrames = 5,index = 0,maxIndex = 2;
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
 
@@ -26,24 +31,48 @@ public class Player extends Entity {
 			}
 			
 			for(int i = 0; i<4; i++){
-				leftPlayer[i] = Game.spritesheet.getSprite(32 + (16*i), 16, 16, 16);
+				leftPlayer[i] = Game.spritesheet.getSprite(96 + (32*i), 0, 32, 32);
 				}
 
 	}
 	public void tick() {
+		moved = false;
 		if(right) {
+			moved = true;
+			dir = dir_right;
 			x+=speed;
 		}
 		else if(left) {
+			moved = true;
+			dir = dir_left;
 			x-=speed;
 		}
 		if(up) { 
+			moved = true;
 			y-=speed;
 		}
 		else if(down) { 
+			moved = true;
 			y+=speed;
 		}
+		if(moved) {
+			frames++;
+			if(frames == maxFrames) {
+				frames = 0;
+				index++;
+				if(index > maxIndex) {
+					index = 0;
+				}
+			}
+		}
 		
+	}
+	public void render(Graphics g) {
+		if(dir == dir_right) {
+		g.drawImage(rightPlayer[index], this.getX(), this.getY(), null);
+		}else if(dir == dir_left) {
+			g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+		}
 	}
 
 }
